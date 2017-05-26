22fecabd1fdd8bf73d70245c2b633bcbbc6af018
newzpanuwat <new_sbh@me.com>
2017-05-27 01:13:57 +0700

    Basic routing

--- server.js
+++ server.js
@@ -11,12 +11,33 @@ var myUser = {
 
 
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'application/json'});
-   // var mySteam = fs.createReadStream(__dirname+"/"+'index.html','utf-8');
-    //mySteam.pipe(res);
+    if (req.url === '/home' || req.url === '/'){
+         res.writeHead(200,{'Content-Type':'text/html'});
+            var mySteam = fs.createReadStream(__dirname+"/"+'index.html','utf-8');
+                mySteam.pipe(res);
+    }
+
+        else if (req.url === '/facebook'){
+            res.writeHead(200,{'Content-Type':'text/html'});
+                var mySteam = fs.createReadStream(__dirname+"/"+'facebook.html','utf-8');
+                    mySteam.pipe(res);
+        }
+
+            else if (req.url === '/youtube'){
+                res.writeHead(200,{'Content-Type':'text/html'});
+                    var mySteam = fs.createReadStream(__dirname+"/"+'youtube.html','utf-8');
+                        mySteam.pipe(res);
+            }
+
+                    else {
+                        res.writeHead(404,{'Content-Type':'text/html'});
+                            var mySteam = fs.createReadStream(__dirname+"/"+'404notfound.html','utf-8');
+                                mySteam.pipe(res);
+                    }
+        
 
     // Display JSON Data
-    res.end(JSON.stringify(myUser));
+    //res.end(JSON.stringify(myUser));
 
 }).listen(8081,'127.0.0.1');
 
