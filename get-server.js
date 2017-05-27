62726127e7c7c34acf4e07a0abc795c520e7bed0
newzpanuwat <new_sbh@me.com>
2017-05-27 19:26:33 +0700

    Body-parser

--- get-server.js
+++ get-server.js
@@ -1,5 +1,9 @@
 var express = require('express')
+var bodyparser = require('body-parser');
+
 var app = express();
+app.use(bodyparser());
+
 
 app.set ('view engine', 'ejs');
 
@@ -7,13 +11,14 @@ app.get ('/showform',function(req,res){
     res.sendFile(__dirname+"/"+"showform.html")
 });
 
-app.get ('/showData',function(req,res){
-    data= {
-        fname : req.query.fname,
-        lname : req.query.lname
+app.post ('/showData',function(req,res){
+    data = {
+        fname : req.body.fname,
+        lname : req.body.lname
     };
     console.log(data);
-    res.end(JSON.stringify(data));
+   // res.end(JSON.stringify(data));
+    res.end('<h1>'+data.fname+'</h1>');
 });
 
 
